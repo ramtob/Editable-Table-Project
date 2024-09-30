1bb549c2b03f5ddbb9deb39cf5f7c9bf4f5e6f2f
Ram Tobolski <ramtob@gmail.com>
2024-09-30 15:27:08 +0300

    Tweaks

--- src/app/editable-table/editable-table.component.ts
+++ src/app/editable-table/editable-table.component.ts
@@ -27,7 +27,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class EditableTableComponent implements OnInit {
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    console.log('EditableTable ngOnInit');
+    // throw new Error('Method not implemented.');
   }
   displayedColumns: string[] = ['name', 'age'];
   dataSource = ELEMENT_DATA;
